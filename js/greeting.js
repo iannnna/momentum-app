736be7bd07e0ecf740910473d5bb648563bc7f6c
iannnna <julianna.barrera03@gmail.com>
2023-03-11 00:35:20 +0800

    styleClock

--- js/greeting.js
+++ js/greeting.js
@@ -1,9 +1,13 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
+const timeFormatButton = document.querySelector("#time-format-button");
 
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
+
+let isClockHovered = false;
+let is24HourFormat = false;
 
 function onLoginSubmit(event) {
   event.preventDefault();
@@ -19,7 +23,7 @@ function getGreeting() {
 
   if (hour >= 6 && hour < 12) {
     return "Good morning";
-  } else if (hour > +12 && hour < 18) {
+  } else if (hour >= 12 && hour < 18) {
     return "Good afternoon";
   } else {
     return "Good Evening";
@@ -27,9 +31,19 @@ function getGreeting() {
 }
 
 function paintGreeting(username) {
-  const greetingText = `${getGreeting()}, ${username}`;
-  greeting.innerText = greetingText;
-  greeting.classList.remove(HIDDEN_CLASSNAME);
+  const now = new Date();
+  const hours = now.getHours();
+  const minutes = now.getMinutes();
+  const seconds = now.getSeconds();
+  const greetingText = getGreeting(hours);
+
+  const clock = document.querySelector("#clock");
+  const greetingContainer = document.querySelector("#greeting-container");
+
+  clock.innerText = `${hours}:${minutes}:${seconds}`;
+  greeting.innerText = `${greetingText}, ${username}`;
+  greetingContainer.classList.remove(HIDDEN_CLASSNAME);
+  clock.style.display = "inline-block";
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
@@ -40,3 +54,17 @@ if (savedUsername === null) {
 } else {
   paintGreeting(savedUsername);
 }
+
+const clock = document.querySelector("#clock");
+
+function getClock() {
+  const date = new Date();
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const seconds = String(date.getSeconds()).padStart(2, "0");
+
+  clock.innerText = `${hours}:${minutes}:${seconds} `;
+}
+
+getClock();
+setInterval(getClock, 1000);

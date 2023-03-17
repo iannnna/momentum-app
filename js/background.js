d3fd05544a623670826ea3a0bd5fb3154d438529
iannnna <julianna.barrera03@gmail.com>
2023-03-18 01:50:18 +0800

    backgroundInterval

--- js/background.js
+++ js/background.js
@@ -1,35 +1,39 @@
-// const API_KEY = "Bps9dbscWkWadBFRQvdjg1zs_-mymsrb4S-uXFRDhS4";
+const API_KEY = "Bps9dbscWkWadBFRQvdjg1zs_-mymsrb4S-uXFRDhS4";
 
-// async function getBackgroundImage() {
-//   let hour = new Date().getHours();
-//   let query = "";
-//   if (hour >= 6 && hour < 12) {
-//     query = "morning";
-//   } else if (hour >= 12 && hour < 18) {
-//     query = "afternoon";
-//   } else if (hour >= 18 && hour < 24) {
-//     query = "evening";
-//   } else {
-//     query = "night";
-//   }
+async function getBackgroundImage() {
+  let hour = new Date().getHours();
+  let query = "";
+  if (hour >= 6 && hour < 12) {
+    query = "morning";
+  } else if (hour >= 12 && hour < 18) {
+    query = "afternoon";
+  } else if (hour >= 18 && hour < 24) {
+    query = "evening";
+  } else {
+    query = "night";
+  }
 
-//   const API_URL = `https://api.unsplash.com/photos/random/?client_id=Bps9dbscWkWadBFRQvdjg1zs_-mymsrb4S-uXFRDhS4&orientation=landscape&crop=entropy&auto=format&width=3840&height=2160&fm=jpg&q=80&nature&peace&query=${query}`;
+  const API_URL = `https://api.unsplash.com/photos/random/?client_id=Bps9dbscWkWadBFRQvdjg1zs_-mymsrb4S-uXFRDhS4&orientation=landscape&crop=entropy&auto=format&width=3840&height=2160&fm=jpg&q=80&nature&peace&query=${query}`;
 
-//   try {
-//     const response = await fetch(API_URL);
-//     const data = await response.json();
-//     return data.urls.regular;
-//   } catch (error) {
-//     console.error(error);
-//     return null;
-//   }
-// }
+  try {
+    const response = await fetch(API_URL);
+    const data = await response.json();
+    return data.urls.regular;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
 
-// async function setRandomBackgroundImage() {
-//   const imageUrl = await getBackgroundImage();
-//   if (imageUrl) {
-//     document.body.style.backgroundImage = `url(${imageUrl})`;
-//   }
-// }
+async function setRandomBackgroundImage() {
+  const imageUrl = await getBackgroundImage();
+  if (imageUrl) {
+    document.body.style.backgroundImage = `url(${imageUrl})`;
+    document.body.style.backgroundSize = "cover";
+    document.body.style.backgroundRepeat = "no-repeat";
+  }
+}
 
-// setRandomBackgroundImage();
+setRandomBackgroundImage();
+
+// setInterval(setRandomBackgroundImage, 10000);
--- js/weather.js
+++ js/weather.js
@@ -1,9 +1,9 @@
-const API_KEY = "07ea8ec2602384af676b3bd1bec9e685";
+const API = "07ea8ec2602384af676b3bd1bec9e685";
 
 function onGeoOk(position) {
   const lng = position.coords.longitude;
   const lat = position.coords.latitude;
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API}&units=metric`;
   fetch(url)
     .then((response) => response.json())
     .then((data) => {

1ea5eaeb55a267fe521ae5aee9d75b9b54e96ba5
iannnna <julianna.barrera03@gmail.com>
2023-03-18 18:17:19 +0800

    backgroundImage

--- js/background.js
+++ js/background.js
@@ -1,39 +1,38 @@
-// const API_KEY = "Bps9dbscWkWadBFRQvdjg1zs_-mymsrb4S-uXFRDhS4";
+const ACCESS_KEY = "Bps9dbscWkWadBFRQvdjg1zs_-mymsrb4S-uXFRDhS4";
 
-// async function getBackgroundImage() {
-//   let hour = new Date().getHours();
-//   let query = "";
-//   if (hour >= 6 && hour < 12) {
-//     query = "morning";
-//   } else if (hour >= 12 && hour < 18) {
-//     query = "afternoon";
-//   } else if (hour >= 18 && hour < 24) {
-//     query = "evening";
-//   } else {
-//     query = "night";
-//   }
+async function getCollectionPhotos(collectionId) {
+  const API_URL = `https://api.unsplash.com/collections/${collectionId}/photos?client_id=${ACCESS_KEY}`;
+  try {
+    const response = await fetch(API_URL);
+    const data = await response.json();
+    return data.map((photo) => photo.urls.regular);
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
 
-//   const API_URL = `https://api.unsplash.com/photos/random/?client_id=Bps9dbscWkWadBFRQvdjg1zs_-mymsrb4S-uXFRDhS4&orientation=landscape&crop=entropy&auto=format&width=3840&height=2160&fm=jpg&q=80&nature&peace&query=${query}`;
+async function setCollectionBackgroundImage() {
+  let hour = new Date().getHours();
+  let collectionId = "";
+  if (hour >= 6 && hour < 12) {
+    collectionId = "qlrE7KfIqH0";
+  } else if (hour >= 12 && hour < 18) {
+    collectionId = "Tk52OHSX4uw";
+  } else if (hour >= 18 && hour < 24) {
+    collectionId = "bQ_TsEMgw2s";
+  } else {
+    collectionId = "UnkoGILoIHw";
+  }
 
-//   try {
-//     const response = await fetch(API_URL);
-//     const data = await response.json();
-//     return data.urls.regular;
-//   } catch (error) {
-//     console.error(error);
-//     return null;
-//   }
-// }
+  const imageUrls = await getCollectionPhotos(collectionId);
+  if (imageUrls) {
+    const imageUrl = imageUrls[Math.floor(Math.random() * imageUrls.length)];
+    document.body.style.backgroundImage = `url(${imageUrl})`;
+    document.body.style.backgroundSize = "cover";
+    document.body.style.backgroundRepeat = "no-repeat";
+  }
+}
 
-// async function setRandomBackgroundImage() {
-//   const imageUrl = await getBackgroundImage();
-//   if (imageUrl) {
-//     document.body.style.backgroundImage = `url(${imageUrl})`;
-//     document.body.style.backgroundSize = "cover";
-//     document.body.style.backgroundRepeat = "no-repeat";
-//   }
-// }
-
-// setRandomBackgroundImage();
-
-// setInterval(setRandomBackgroundImage, 10000);
+setCollectionBackgroundImage();
+// setInterval(setCollectionBackgroundImage, 60000);
